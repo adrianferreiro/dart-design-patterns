75735ba0cba2c2b5e366ade6e1fdb9681495f06a
Adrian Ferreiro <adriferreiro.88@gmail.com>
2025-07-10 12:01:41 -0300

    Behavioral: add comments to state pattern

--- bin/behavioral/state.dart
+++ bin/behavioral/state.dart
@@ -1,6 +1,23 @@
-// ! Patrón State en Dart
-// Este patrón permite cambiar el comportamiento de un objeto cuando su estado interno cambia
-// Es útil cuando el objeto necesita reaccionar diferente según su estado actual
+/**
+ * ! Patrón State
+ * Este patrón permite a un objeto cambiar su comportamiento
+ * cuando su estado interno cambia.
+ *
+ * * Es útil cuando un objeto tiene un comportamiento que depende de su estado
+ * * y debe cambiar su comportamiento en tiempo de ejecución dependiendo de ese estado.
+ *
+ * https://refactoring.guru/es/design-patterns/state
+ */
+
+/**
+ * * Objetivo: Implementar el patrón State para simular el funcionamiento
+ * * de una máquina expendedora.
+ * * La máquina tiene diferentes estados,
+ *  * Como Esperando Dinero,
+ *  * Seleccionando Producto,
+ *  * Entregando Producto,
+ * * y su comportamiento varía dependiendo del estado actual.
+ */
 
 import 'dart:io';
 import 'dart:async';
